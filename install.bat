2b6511f119bc48a2a244d58fa28ad184f5989f72
kirill@pc <kirvilkov@gamil.com>
2023-08-11 19:12:49 +0300

    update installation

--- install.bat
+++ install.bat
@@ -3,4 +3,4 @@ call .\venv\Scripts\activate.bat
 python.exe -m pip install --upgrade pip
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
-python loadModel.py
+python install.py
